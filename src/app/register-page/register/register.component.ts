5df345cea2a034c9db91a3c7704c46a3c2b72a5c
Jad El Nader <Jadelnader99@gmail.com>
2024-07-29 20:21:05 +0200

    checkExistingMail at register

--- src/app/register-page/register/register.component.ts
+++ src/app/register-page/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { LoginHeaderComponent } from "../../login-page/login-header/login-header.component";
 import { LoginFooterComponent } from "../../login-page/login-footer/login-footer.component";
 import { FormsModule } from '@angular/forms';
@@ -7,9 +7,10 @@ import { CommonModule } from '@angular/common';
 
 
 import { trigger, transition, query, style, animate, group } from '@angular/animations';
+import { AuthenticationService } from '../../services/authentication.service';
 
 const left = [
-    query(':enter, :leave', style({ position: 'absolute',  }), { optional: true }),
+    query(':enter, :leave', style({ position: 'absolute', }), { optional: true }),
     group([
         query(':enter', [style({ transform: 'translateX(-100%)' }), animate('.3s ease-out', style({ transform: 'translateX(0%)' }))], {
             optional: true,
@@ -46,8 +47,17 @@ const right = [
 })
 export class RegisterComponent {
     sharedService = inject(SharedService);
+    authService = inject(AuthenticationService);
     counter: number = 0;
     steps: Array<number> = [1, 2, 3];
+    firstName!: string;
+    lastName!: string;
+    address!: string;
+    phoneNumber!: string;
+    @ViewChild('mailInfo') mailInfo!: ElementRef<HTMLParagraphElement>;
+ 
+
+
 
     onNext() {
         if (this.counter < this.steps.length - 1) {
@@ -59,6 +69,37 @@ export class RegisterComponent {
         if (this.counter > 0) {
             this.counter--;
         }
+    }
+
+
+    async checkExistMail() {
+        await this.authService.checkUsername(this.sharedService.currentMail).then(mailExist => {
+            if (mailExist) {
+                this.mailInfo.nativeElement.style.opacity = '1'
+            } else {
+                this.mailInfo.nativeElement.style.opacity = '0'
+                this.onNext()
+                console.log();
+                
+            }
+        }).catch(error => {
+            console.error('Error:', error);
+            // Optional: Behandle den Fehler hier
+        });
+    }
+
+
+
+    register() {
+        let test = {
+            'address': this.address,
+            'number': this.phoneNumber,
+            'firstName': this.firstName,
+            'lastName': this.lastName,
+            'mail': this.sharedService.currentMail
+        }
+        console.log(test);
+
     }
 }
 
