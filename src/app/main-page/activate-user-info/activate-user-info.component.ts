007419e8763692e677de1d6ce5dc300254523f43
Jad El Nader <Jadelnader99@gmail.com>
2024-08-01 18:56:38 +0200

    design acitvate info component

--- src/app/main-page/activate-user-info/activate-user-info.component.ts
+++ src/app/main-page/activate-user-info/activate-user-info.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationService } from '../../services/authentication.service';
+import { SharedService } from '../../services/shared.service';
+
 
 @Component({
   selector: 'app-activate-user-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './activate-user-info.component.html',
   styleUrl: './activate-user-info.component.scss'
 })
 export class ActivateUserInfoComponent {
-
+  sharedService = inject(SharedService);
 }
