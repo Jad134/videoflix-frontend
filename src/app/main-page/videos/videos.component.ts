1cc1d5a216640a05361e75efe8771cc685e50e9f
Jad El Nader <Jadelnader99@gmail.com>
2024-08-08 19:24:13 +0200

    sort videos and category

--- src/app/main-page/videos/videos.component.ts
+++ src/app/main-page/videos/videos.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { VideoService } from '../../services/video.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-videos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './videos.component.html',
   styleUrl: './videos.component.scss'
 })
 export class VideosComponent {
+
+
+  videoService = inject(VideoService)
+  videos: any[] = [];
+  groupedVideos: { [key: string]: any[] } = {};
+
+
+  ngOnInit(): void {
+    this.videoService.getVideos().subscribe(data => {
+      this.videos = data;
+      this.groupVideosByCategory();
+    });
+  }
+
+  groupVideosByCategory(): void {
+    this.groupedVideos = this.videos.reduce((groups, video) => {
+      const category = video.category;
+      if (!groups[category]) {
+        groups[category] = [];
+      }
+      groups[category].push(video);
+      console.log(groups);
+      
+      return groups;
+    
+    }, {});
+  }
+
+
+  getCategories(): string[] {
+    return Object.keys(this.groupedVideos);
+  }
+
 
 }
