527d3d2696017d5d29dfcb1f632f062ae0e4a2f7
unknown <jadelnader99@gmail.com>
2024-10-07 13:58:00 +0200

    add loadingscreen to startpage and keydown enter at startpage

--- src/app/login-page/login-main/login-main/login-main.component.ts
+++ src/app/login-page/login-main/login-main/login-main.component.ts
@@ -15,18 +15,20 @@ import { AuthenticationService } from '../../../services/authentication.service'
 import { Router } from '@angular/router';
 import { LoginHeaderComponent } from "../../login-header/login-header.component";
 import { SharedService } from '../../../services/shared.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-login-main',
     standalone: true,
     templateUrl: './login-main.component.html',
     styleUrl: './login-main.component.scss',
-    imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatIconModule, LoginHeaderComponent]
+    imports: [CommonModule,FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatIconModule, LoginHeaderComponent]
 })
 export class LoginMainComponent {
   authService = inject(AuthenticationService);
   sharedService = inject(SharedService)
   email: any;
+  loading :boolean = false;
 
   constructor(private router: Router){
 
@@ -36,18 +38,22 @@ export class LoginMainComponent {
    * This function check, if the mail already exist at the database
    */
   async checkIfMailExist() {
+    this.loading = true
     await this.authService.checkUsername(this.email).then(mailExist => {
         if (mailExist) {
           this.router.navigate(['login'])
             console.log("Die E-Mail ist bereits vergeben.");
             this.sharedService.currentMail = this.email;
+            this.loading = false
         } else {
           this.router.navigate(['register'])
             console.log("Die E-Mail ist verfügbar.");
             this.sharedService.currentMail = this.email;
+            this.loading = false
         }
     }).catch(error => {
         console.error('Error:', error);
+        this.loading = false
     });
 }
 }
